b05d04b370df6ad6e67a215e2b2e77a830967b2e
rhkanrdl <emrdl7@gmail.com>
2021-09-23 07:44:05 +0000

    달력추가

--- source/js/common.js
+++ source/js/common.js
@@ -1,6 +1,5 @@
 
 
-
 $(function () {
 
 
@@ -10,7 +9,7 @@ $(function () {
 
 
 
-
+try {
     $('ul.list').niceScroll({
         emulatetouch:true
       });
@@ -30,7 +29,7 @@ $(function () {
             type: 'bullets',
         }
     });
-
+} catch(e) {}
 
 
     var keypadtarget = $("input:focus").attr("id");
@@ -57,5 +56,37 @@ $(function () {
             $("input#" + keypadtarget).val($("input#" + keypadtarget).val() + $(this).text());
         }
     });
+
+
+
+    $.fn.datepicker.language['ko'] = {
+        days: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+        daysShort: ['일', '월', '화', '수', '목', '금', '토'],
+        daysMin: ['일', '월', '화', '수', '목', '금', '토'],
+        months: ['1월','2월','3월','4월','5월','6월', '7월','8월','9월','10월','11월','12월'],
+        monthsShort: ['1월','2월','3월','4월','5월','6월', '7월','8월','9월','10월','11월','12월'],
+        today: '오늘',
+        clear: '초기화',
+        dateFormat: 'yyyy-mm-dd',
+        timeFormat: 'hh:ii aa',
+        firstDay: 0
+    };
+
+
+
+// Initialization
+$('.start-date').datepicker({
+    language: "ko"
+});
+// Access instance of plugin
+$('.start-date').data('datepicker');
+
+// Initialization
+$('.end-date').datepicker({
+    language: "ko"
+});
+// Access instance of plugin
+$('.end-date').data('datepicker');
+
 
 });
